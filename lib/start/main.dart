57a3ff8b1d4d0146d8d19c752d4b57d9e25d50c6
rubyzera <fabiocr6@hotmail.com>
2022-10-04 19:34:30 -0300

    Last commit

--- lib/start/main.dart
+++ lib/start/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import '../routes.dart';
 import 'package:pet_walk/home/home.dart';
+import 'package:get/get.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       initialRoute: RouteGenerator.home,
       onGenerateRoute: RouteGenerator.generateRoute,
